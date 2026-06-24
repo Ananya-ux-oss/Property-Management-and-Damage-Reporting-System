b5f76e2619583b89c7ced2387bf9d4f02155e5d7
haileyesusdagne-source <haileyesus.dagne@bitscollege.edu.et>
2026-06-24 21:10:30 +0300

    Update UnauthorizedException.java

--- exception/UnauthorizedException.java
+++ exception/UnauthorizedException.java
@@ -1,3 +1,5 @@
+package exception;
+
 public class UnauthorizedException extends SystemException {
     public UnauthorizedException(String role, String action) {
         super("Role '" + role + "' is not authorized to: " + action);
